592819ee3f3d892a6b1690b0b4490dd6b080cb11
ttmmd55 <chenshuting47@gmail.com>
2023-04-28 16:39:57 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 import * as THREE from 'three';
-		import { ARButton } from 'three/addons/webxr/ARButton.js';
-		import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
 		let camera, scene, renderer;
 		let controller;
 
-		const geometries = [
-			new THREE.BoxGeometry(0.04, 0.04, 0.04).rotateX( Math.PI / 2 ),
-			new THREE.ConeGeometry(0.04, 0.04, 64).rotateX( Math.PI / 2 ),
-			new THREE.CylinderGeometry(0.04, 0.04, 0.1, 5).rotateX( Math.PI / 2 ),
-			new THREE.CylinderGeometry(0.04, 0.04, 0.04, 64).rotateX( Math.PI / 2 ),
-			new THREE.IcosahedronGeometry(0.04, 8).rotateX( Math.PI / 2 ),
-			new THREE.IcosahedronGeometry(0.04, 4).rotateX( Math.PI / 2 ),
-			new THREE.TorusGeometry(0.04, 0.008, 64, 32).rotateX( Math.PI / 2 )
-		];
+		const gltfLoader = new GLTFLoader();
+		const url = 'assets/cafd.gltf';
+		gltfLoader.load(url, (gltf) => {
+		  const root = gltf.scene;
+		  scene.add(root);
+		});
+		  
+		// const geometries = [
+		// 	new THREE.BoxGeometry(0.04, 0.04, 0.04).rotateX( Math.PI / 2 ),
+		// 	new THREE.ConeGeometry(0.04, 0.04, 64).rotateX( Math.PI / 2 ),
+		// 	new THREE.CylinderGeometry(0.04, 0.04, 0.1, 5).rotateX( Math.PI / 2 ),
+		// 	new THREE.CylinderGeometry(0.04, 0.04, 0.04, 64).rotateX( Math.PI / 2 ),
+		// 	new THREE.IcosahedronGeometry(0.04, 8).rotateX( Math.PI / 2 ),
+		// 	new THREE.IcosahedronGeometry(0.04, 4).rotateX( Math.PI / 2 ),
+		// 	new THREE.TorusGeometry(0.04, 0.008, 64, 32).rotateX( Math.PI / 2 )
+		// ];
 
 		init();
 		animate();
@@ -53,7 +59,7 @@ import * as THREE from 'three';
 			//
 
 			function onSelect() {
-				const geometry = geometries[Math.floor(Math.random() * geometries.length)];
+				//const geometry = geometries[Math.floor(Math.random() * geometries.length)];
 				// const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
 				// const material = new THREE.MeshPhysicalMaterial({
 				// 	color: new THREE.Color().setHSL(Math.random(), 1, 0.5),
@@ -63,7 +69,7 @@ import * as THREE from 'three';
 					const material = new THREE.MeshPhysicalMaterial({
 					color: new THREE.Color().setHSL(Math.random(), 1, 0.5)
 				});
-				const mesh = new THREE.Mesh(geometry, material);
+				const mesh = new THREE.Mesh(gltfLoader, material);
 				mesh.position.set(0, 0, - 0.3).applyMatrix4(controller.matrixWorld);
 				mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
 				scene.add(mesh);
