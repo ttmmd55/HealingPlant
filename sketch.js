0ad734104e2ffce05ef8312cf9b6b711198cb1e4
ttmmd55 <chenshuting47@gmail.com>
2023-04-30 01:12:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,125 +1,72 @@
 import * as THREE from 'three';
-import { ARButton } from 'three/addons/webxr/ARButton.js';
-import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-		let camera, scene, renderer;
-		let controller;
+let camera, scene, renderer;
+let controller;
+let model;
 
-		
-		
+init();
+animate();
 
-		init();
-		animate();
+function init() {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
 
-		
+  scene = new THREE.Scene();
 
-		function init() {
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
-			const container = document.createElement('div');
-			document.body.appendChild(container);
+  const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  light.position.set(0.5, 1, 0.25);
+  scene.add(light);
+  
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(0, 1, 0);
+  scene.add(directionalLight);
 
-			scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.xr.enabled = true;
+  container.appendChild(renderer.domElement);
 
-			camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+  document.body.appendChild(ARButton.createButton(renderer));
 
-			const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-			light.position.set(0.5, 1, 0.25);
-			scene.add(light);
-			// const lightB = new THREE.DirectionalLight()
-			// lightB.castShadow = true
-			// lightB.shadow.mapSize.width = 640
-			// lightB.shadow.mapSize.height = 480
-			// lightB.shadow.camera.near = 0.5
-			// lightB.shadow.camera.far = 100
-			// scene.add(lightB);
-			const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-			directionalLight.position.set(0, 1, 0);
-			scene.add(directionalLight);
+  const loader = new GLTFLoader();
 
+  loader.load('assets/tree2.0.glb', function (gltf) {
+    model = gltf.scene;
+    scene.add(model);
+  });
 
-			//
+  controller = renderer.xr.getController(0);
+  controller.addEventListener('select', onSelect);
+  scene.add(controller);
 
-			renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			renderer.xr.enabled = true;
-			container.appendChild(renderer.domElement);
+  window.addEventListener('resize', onWindowResize);
+}
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-			//
-			document.body.appendChild(ARButton.createButton(renderer));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-			//
-			// const gltfLoader = new GLTFLoader();
-			// const url = 'assets/tree2.0.glb';
-			// gltfLoader.load(url, (gltf) => {
-			// const tree = gltf.scene;
-			// scene.add(tree);
-			// });
+function onSelect() {
+  if (model) {
+    const modelInstance = model.clone();
+    modelInstance.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
+    modelInstance.quaternion.setFromRotationMatrix(controller.matrixWorld);
+    scene.add(modelInstance);
+  }
+}
 
-			const loader = new THREE.GLTFLoader();
+function animate() {
+  renderer.setAnimationLoop(render);
+}
 
-			loader.load( 'assets/tree2.0.glb', function ( gltf ) {
-			
-				const model = gltf.scene;
-				scene.add( model );
-			
-			});
-			
-			function onSelect() {
-				//const leaf = gltfLoader[Math.floor(Math.random() * gltfLoader.length)];
-				//const geometry = geometries[Math.floor(Math.random() * geometries.length)];
-				// const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
-				// const material = new THREE.MeshPhysicalMaterial({
-				// 	color: new THREE.Color().setHSL(Math.random(), 1, 0.5),
-				// 	metalness: 1,
-				// 	roughness: 0.1
-				// });
-				// 	const material = new THREE.MeshPhysicalMaterial({
-				// 	color: new THREE.Color().setHSL(Math.random(), 1, 0.5)
-				// });
-
-				
-
-				//const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-				// const mesh = new THREE.Mesh(gltfLoader);//, material);
-				// mesh.position.set(0, 0, - 10).applyMatrix4(controller.matrixWorld);
-				// mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
-				// scene.add(mesh);
-				const modelInstance = model.clone();
-				modelInstance.position.set( 0, 0, - 0.1 ).applyMatrix4( controller.matrixWorld );
-				modelInstance.quaternion.setFromRotationMatrix( controller.matrixWorld );
-				scene.add( modelInstance );
-			}
-
-			controller = renderer.xr.getController(0);
-			controller.addEventListener('select', onSelect);
-			scene.add(controller);
-
-			//
-
-			window.addEventListener('resize', onWindowResize);
-
-		}
-
-		function onWindowResize() {
-
-			camera.aspect = window.innerWidth / window.innerHeight;
-			camera.updateProjectionMatrix();
-
-			renderer.setSize(window.innerWidth, window.innerHeight);
-
-		}
-
-		//
-	
-		function animate() {
-			renderer.setAnimationLoop(render);
-
-		}
-
-		function render() {
-
-			renderer.render(scene, camera);
-
-		}
+function render() {
+  renderer.render(scene, camera);
+}
