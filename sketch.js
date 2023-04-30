d8fe79e71a3242c49d19ad4c2e1c2abb043e53be
ttmmd55 <chenshuting47@gmail.com>
2023-04-30 01:45:45 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,6 +62,7 @@ import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 			
 			function onSelect() {
 				const modelInstance = modelInstances[modelInstances.length - 1].clone();
+				modelInstance.scale.set(0.67, 0.67, 0.67);
 				modelInstance.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
 				modelInstance.quaternion.setFromRotationMatrix(controller.matrixWorld);
 				scene.add(modelInstance);
